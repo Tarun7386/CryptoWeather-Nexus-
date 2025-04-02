7f3d541354f42b1633be1d97f22333331abe6386
Tarun7386 <nellikudurutarun1234@gmail.com>
2025-04-03 01:31:20 +0530

    Configure Next.js settings  image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'openweathermap.org', // For weather icons
+      'assets.coingecko.com', // For crypto icons
+      'newsdata.io' ,// For news thumbnails
+      'coin-images.coingecko.com',
+    ],
+  },
+  // Optional: Enable SWC minification for better performance
+  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
