b84d852552abd497d3764daca11f5656530383e9
Tarun7386 <nellikudurutarun1234@gmail.com>
2025-04-04 11:12:45 +0530

    Added Providers component for Redux store integration

---
+++ src/app/providers.tsx
@@ -0,0 +1,8 @@
+"use client"; // ✅ This ensures it's a Client Component
+
+import { Provider } from "react-redux";
+import  store  from "@/redux/store";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
